a57eacf00d15a62298ac9571dc596327467b78f1
Qz <quadratz@proton.me>
2026-02-15 17:16:30 +0700

    fix: :bug: ignore dist folder
    Signed-off-by: Qz <quadratz@proton.me>

--- eslint.config.ts
+++ eslint.config.ts
@@ -11,6 +11,7 @@ export default defineConfig(
   tseslint.configs.stylisticTypeChecked,
   prettierConfig,
   {
+    ignores: ["dist/**"],
     languageOptions: {
       parserOptions: {
         projectService: true,
--- template/eslint.config.ts
+++ template/eslint.config.ts
@@ -1,19 +1,21 @@
-import { defineConfig, globalIgnores } from "eslint/config";
+import type { Config } from "eslint/config";
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 import prettierConfig from "eslint-config-prettier";
+import { defineConfig, globalIgnores } from "eslint/config";
 
 export default defineConfig(
   globalIgnores(["./template/**"]),
   eslint.configs.recommended,
   tseslint.configs.strictTypeChecked,
   tseslint.configs.stylisticTypeChecked,
+  prettierConfig,
   {
+    ignores: ["dist/**"],
     languageOptions: {
       parserOptions: {
         projectService: true,
       },
     },
   },
-  prettierConfig,
-);
+) as Config;
